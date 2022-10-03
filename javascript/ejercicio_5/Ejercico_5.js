cdb8a45daceace5b79771cb4e81d8430a842220c
alejandro asess <101218762+aleaases@users.noreply.github.com>
2022-10-03 17:32:54 -0300

    main comiit ejejrcicio 5 completo

---
+++ javascript/ejercicio_5/Ejercico_5.js
@@ -0,0 +1,12 @@
+let Altura_cen = 178;
+let  Altura_met = 1.78;
+let Peso = 90.5;
+
+let  Altura_redo = Math.ceil(Altura_met);
+console.log(Altura_redo);
+
+let Peso_redo = Math.floor(Peso);
+console.log (Peso_redo);
+
+let Igual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(Igual)
