fae59773331f6e564badc368de1683f7fa4c4a97
alejandro asess <101218762+aleaases@users.noreply.github.com>
2022-10-03 13:09:20 -0300

    main ejercico 4 completo

--- javascript/ejercicio_4/ejercicio-4.js
+++ javascript/ejercicio_4/ejercicio-4.js
@@ -1,6 +1,8 @@
-let str = "hola soy una string"
-console. log(str.length)
-
-
-let slice_str = str.slice(0, 10)
-console. log(slice_str)
+let texto_largo = "este es un texto de prueba para probar la teoia y la gramatica de los archivos javascripts la manera de hacerlos es la siguiente"
+//esta es una forma de saber cuantas veces esta una string en un texto
+console.log(texto_largo.match(/la/g))
+// esta es una forma de verificar que string hay en un texto
+console.log(texto_largo.includes("perro"))
+// comprueba si empieza y termina con una frase del texto trabajado
+console.log(texto_largo.startsWith("este"))
+console.log(texto_largo.endsWith("siguiente"))
