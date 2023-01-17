c8b9997b2def9b17e4cb8d3e65117b8f9eb7dcb0
alejandro asess <101218762+aleaases@users.noreply.github.com>
2023-01-16 23:05:20 -0300

    master commit ejercicio 10

---
+++ javascript/ejercicio_10/index.js
@@ -0,0 +1,12 @@
+import {suma, multiplica} from './modulos/controller.js'
+
+let resultado = multiplica(suma(1.2),suma(4,5))
+console.log(resultado)
+
+
+import chalk from 'chalk';
+
+console.log(chalk.green(resultado));
+
+
+
